830b28e7bf43b02f12d9a92924406b5831eb090c
Ziad Saoud <69092782+ZiadSaoud@users.noreply.github.com>
2021-09-27 00:01:32 +0300

    Update main.java

--- Main/main.java
+++ Main/main.java
@@ -1,1 +1,58 @@
-//main file
+package SmartAgent;
+	
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	private double xOffset=0;
+	private double yOffset=0;
+	 @FXML
+	 private Button createEnv;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("UI.fxml"));
+			Scene scene = new Scene(root,1000,800);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(true);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.show();
+			root.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+					
+				}
+			});
+			root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent arg0) {
+					primaryStage.setX(arg0.getScreenX()-xOffset);
+					primaryStage.setY(arg0.getScreenY()-yOffset);
+					
+				}
+			});
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+

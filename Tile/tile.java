9fc3af42b518d17bcf9ce455468e1ba6451e953e
Ziad Saoud <69092782+ZiadSaoud@users.noreply.github.com>
2021-09-27 00:05:08 +0300

    Add files via upload

---
+++ Tile/tile.java
@@ -0,0 +1,112 @@
+package SmartAgent;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+public class tile extends Button implements Comparable<tile>{
+	private boolean black = false;
+	private boolean img = false;
+	private int X;
+	private int Y;
+	private tile up = null;
+	private tile down = null;
+	private tile left = null;
+	private tile right = null;
+	private int weight=Integer.MAX_VALUE;
+	public tile(int x,int y, String id, Controller c) {
+		X=x;
+		Y=y;
+		setLayoutX(x);
+		setLayoutY(y);
+		setId(id);
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				if(!c.disableTiles()){
+				if(c.checkAgent() && !black) {
+					c.setAgent(X+25, Y+25,tile.this);
+					
+				}else if(!c.checkAgent()){
+					
+				if(arg0.getButton() == MouseButton.PRIMARY){
+					if(!black && !img) {
+					setStyle("-fx-background-image:url('dust.png')");
+					img=true;
+					c.setDestination(tile.this);
+					}else if(!black && img) {
+						setStyle("-fx-background-image:none");
+						img=false;
+						c.removeDestination(tile.this);
+					}
+				}
+				if(arg0.getButton() == MouseButton.SECONDARY) {
+					setStyle("-fx-background-color: black");
+					black = true;
+				}
+				if(arg0.getClickCount() == 2) {
+					setStyle("-fx-background-image:none");
+					black = false;
+					img = false;
+					c.removeDestination(tile.this);
+				}
+				
+				}
+			}
+			}
+		});	
+	}
+	public int getX() {
+		return X;
+	}
+	
+	public int getY() {
+		return Y;
+	}
+	
+	public tile getUp() {
+		return up;
+	}
+	public void setUp(tile up) {
+		this.up = up;
+	}
+	public tile getDown() {
+		return down;
+	}
+	public void setDown(tile down) {
+		this.down = down;
+	}
+	public tile getLeft() {
+		return left;
+	}
+	public void setLeft(tile left) {
+		this.left = left;
+	}
+	public tile getRight() {
+		return right;
+	}
+	public void setRight(tile right) {
+		this.right = right;
+	}
+	public boolean getWall() {
+		return black;
+	}
+	public int getW() {
+		return weight;
+	}
+	public void setW(int w) {
+		this.weight = w;
+	}
+	
+	@Override
+	public int compareTo(tile n) {
+		if(weight<n.weight) {
+			return -1;
+		}else if(weight>n.weight) {
+			return 1;
+		}
+		return 0;
+	}
+	
+}
